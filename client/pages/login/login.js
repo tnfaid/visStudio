2155a7acea32e64d4cabf98f1d422f584bca3b3d
tnfaid <nurfaidahtitik@gmail.com>
2017-08-24 12:01:31 +0700

    add new form

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -1,5 +1,41 @@
+var myLogoutFunt = function(){
+	FlowRouter.go('/');
+}
+
 Template.login.events({
   'click button'() {
-    Meteor.logout();
+    AccountsTemplates.logout();
   },
 });
+
+AccountsTemplates.configure({
+	confirmPassword: false,
+	termsUrl: 'terms-of-use',
+	privacyUrl:'privacy',
+	onLogoutHook: myLogoutFunt
+})
+
+AccountsTemplates.addFields([
+ {
+  	_id: 'firstName',
+  	type: 'text',
+  	display: 'First Name',
+  	required: true,
+  	re: /(?=.*[a-z])(?=.*[A-Z])/,
+  	errStr: '1 lowercase and 1 uppercase letter required'
+  },
+  {
+  	_id: 'gender',
+  	type: 'select',
+  	displayName: 'Gender',
+  	select: [
+  		{
+  			text: 'Male',
+  			value: 'male'
+  		}, {
+  			text: 'Female',
+  			value: 'female'
+  		}
+  	]
+  }
+]);
